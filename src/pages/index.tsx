e444a18c6eec7004d486b754b50649dacf967abc
ui-d <dawiddeveloper@gmail.com>
2023-04-26 18:05:18 +0200

    fix: minor fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useSession } from '@supabase/auth-helpers-react';
 import * as React from 'react';
 
+import { Container } from '@/components/containers/Container';
 import { Layout } from '@/components/layout/Layout';
 import { Seo } from '@/components/Seo';
 
@@ -13,10 +14,16 @@ const HomePage = (): JSX.Element => {
         <></>
       ) : (
         <Layout>
-          {/* <Seo templateTitle='Home' /> */}
+          <Seo templateTitle='Dashboard' />
           <Seo />
 
-          <main className='mb-52'></main>
+          <main className='mb-52'>
+            <>
+              <Container>
+                <div className='flex w-full grid-flow-row grid-cols-12  gap-4 overflow-y-hidden overflow-x-scroll px-10 pb-10 pt-1 xl:grid xl:overflow-x-auto xl:px-4'></div>
+              </Container>
+            </>
+          </main>
         </Layout>
       )}
     </>
